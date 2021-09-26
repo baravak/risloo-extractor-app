141dd0d4320002a0d9b0a16e034464036dc65362
Mahdi <amirabadi.mahdi@gmail.com>
2021-09-26 22:48:28 +0330

    cli improved & measure.js implemented as test

--- src/cli.js
+++ src/cli.js
@@ -1,139 +1,131 @@
-import arg from "arg";
+import { Command, Option } from "commander/esm.mjs";
 import fs from "fs/promises";
-import inquirer from "inquirer";
+import { access, constants } from "fs";
 import path from "path";
 import sharp from "sharp";
 import { Buffer } from "buffer";
 const eta = require("eta");
+
+const program = new Command();
 
 eta.configure({
   views: path.join(__dirname, "..", "views", "profiles"),
 });
 
 function parseArgumentsIntoOptions(rawArgs) {
-  const args = arg(
-    {},
-    {
-      argv: rawArgs.slice(2),
-    }
+  let output = {};
+
+  program
+    .command("draw")
+    .alias("D")
+    .argument("<profileName>")
+    .description("You can draw a profile using this command!")
+    .addOption(
+      new Option("-i, --input-type <type>", "input type")
+        .choices(["local", "remote", "raw_json"])
+        .makeOptionMandatory()
+    )
+
+    .addOption(
+      new Option("-d, --input-data <data>", "input data").makeOptionMandatory()
+    )
+
+    .addOption(
+      new Option("-o, --output-type <type>", "output type")
+        .choices(["local", "remote"])
+        .makeOptionMandatory()
+    )
+
+    .addOption(
+      new Option(
+        "-a, --output-address <address>",
+        "output address"
+      ).makeOptionMandatory()
+    )
+
+    .addOption(
+      new Option("-m, --measure", "measure feature in SVG").default(false)
+    )
+
+    .action((profileName, options, command) => {
+      output = {
+        command: command.name(),
+        profileName,
+        ...options,
+      };
+    });
+
+  program.name("risloo").usage("draw <profileName> [options]");
+
+  program.showHelpAfterError();
+
+  program.showSuggestionAfterError();
+
+  program.parse(rawArgs);
+
+  return output;
+}
+
+async function draw(options) {
+  // Suppose that both input & output type are "local"
+
+  let profilePath = path.join(
+    __dirname,
+    "profiles",
+    `${options.profileName}.js`
   );
-  return {
-    chartType: args._[0],
-    jsonFile: args._[1],
-    saveDir: args._[2],
-  };
-}
 
-async function promptForMissingOptions(options) {
-  const questions = [];
-  if (!options.chartType) {
-    questions.push({
-      type: "input",
-      name: "chartType",
-      message: "Please specify the type of chart you want?\n",
+  access(options.inputData, constants.F_OK, async (err) => {
+    if (err) {
+      console.log("Input Data File Does Not Exist!");
+      return;
+    }
+
+    const dataset = JSON.parse(await fs.readFile(options.inputData, "utf8"));
+
+    access(profilePath, constants.F_OK, async (err) => {
+      if (err) {
+        console.log("Profile Name Is Not Valid!");
+        return;
+      }
+
+      const profileClass = require(profilePath);
+      const profileObj = new profileClass(dataset);
+      const ctx = {
+        ...profileObj.getTemplateEngineParams(),
+        measure: options.measure,
+      };
+
+      const xml = await eta.renderFile(options.profileName, ctx);
+
+      const mapObj = {
+        'text-anchor="start"': 'text-anchor="end"',
+        'text-anchor="end"': 'text-anchor="start"',
+      };
+
+      const svg = xml.replace(
+        /text-anchor="start"|text-anchor="end"/g,
+        (matched) => mapObj[matched]
+      );
+
+      const buf = Buffer.from(xml, "utf8");
+      const png = await sharp(buf, { density: 500 });
+
+      await fs.writeFile(
+        `${options.outputAddress}/${options.profileName}.svg`,
+        svg
+      );
+      await png.toFile(`${options.outputAddress}/${options.profileName}.png`);
     });
-  }
-
-  if (!options.jsonFile) {
-    questions.push({
-      type: "input",
-      name: "jsonFile",
-      message: "Please determine the address of input json file!\n",
-    });
-  }
-
-  if (!options.saveDir) {
-    questions.push({
-      type: "input",
-      name: "saveDir",
-      message: "You missed to specify the save directory...\n",
-    });
-  }
-
-  const answers = await inquirer.prompt(questions);
-  return {
-    chartType: options.chartType || answers.chartType,
-    jsonFile: options.jsonFile || answers.jsonFile,
-    saveDir: options.saveDir || answers.saveDir,
-  };
-}
-
-async function promptForIncorrectInput(options, incorrectInput) {
-  const questions = [];
-  if (incorrectInput.chartType) {
-    questions.push({
-      type: "input",
-      name: "chartType",
-      message: `${options.chartType} chart doesn't exist.\nPlease enter a correct chart type!\n`,
-    });
-  }
-
-  if (incorrectInput.jsonFile) {
-    questions.push({
-      type: "input",
-      name: "jsonFile",
-      message: `${options.jsonFile} doesn't exist.\nPlease enter a correct JSON file path...\n`,
-    });
-  }
-
-  const answers = await inquirer.prompt(questions);
-
-  return {
-    chartType: answers.chartType || options.chartType,
-    jsonFile: answers.jsonFile || options.jsonFile,
-    saveDir: answers.saveDir || options.saveDir,
-  };
+  });
 }
 
 export async function cli(args) {
   let options = parseArgumentsIntoOptions(args);
-  options = await promptForMissingOptions(options);
-  let incorrectInput;
-  let dataset = [];
-  let ctx = {};
-  let profileClass = [];
 
-  while (true) {
-    incorrectInput = {};
-    try {
-      dataset = JSON.parse(await fs.readFile(options.jsonFile, "utf8"));
-    } catch (err) {
-      incorrectInput["jsonFile"] = true;
-    }
-
-    try {
-      profileClass = require(path.join(
-        __dirname,
-        "profiles",
-        options.chartType
-      ));
-    } catch (err) {
-      incorrectInput["chartType"] = true;
-    }
-
-    // try {
-      const profileObj = new profileClass(dataset);
-      ctx = profileObj.getTemplateEngineParams();
-    // } catch (err) {
-    //   console.log("An Error Occured due to Dataset Issues!");
-    // }
-
-    if (!Object.keys(incorrectInput).length) break;
-    options = await promptForIncorrectInput(options, incorrectInput);
+  switch (options.command) {
+    case "draw":
+      draw(options);
+      break;
   }
-
-  const xml = await eta.renderFile(options.chartType, ctx);
-
-  const mapObj = {
-    'text-anchor="start"': 'text-anchor="end"',
-    'text-anchor="end"': 'text-anchor="start"',
-  }
-
-  const svg = xml.replace(/text-anchor="start"|text-anchor="end"/g, (matched) => mapObj[matched])
-
-  const buf = Buffer.from(xml, 'utf8');
-  const png = await sharp(buf, { density: 500 });
-
-  await fs.writeFile(`${options.saveDir}/${options.chartType}.svg`, svg);
-  await png.toFile(`${options.saveDir}/${options.chartType}.png`);
 }

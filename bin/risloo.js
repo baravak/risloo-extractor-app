081fc03470df8cf09d66d8121378db50eb055df1
AmirABody <amirabadi.mahdi@gmail.com>
2021-10-17 21:48:05 +0330

    CLI command entry corrected

--- bin/risloo.js
+++ bin/risloo.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 require = require("esm")(module /*, options*/);
-require("./src/cli").cli(process.argv);
+require("../src/cli").cli(process.argv);
 
 process.on("unhandledRejection", (reason, promise) => {
   console.log("Unhandled Rejection at:", promise, "reason:", reason);
-})
+});

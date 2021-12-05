584c74c8a5ecc7f104a6a58400d4475b5f52de24
Mahdi Amirabadi <amirabadi.mahdi@gmail.com>
2021-12-05 10:20:41 +0330

    Console errors

--- bin/risloo.js
+++ bin/risloo.js
@@ -2,7 +2,7 @@
 
 require("../src/cli")
   .cli(process.argv)
-  .catch((err) => {});
+  .catch((err) => {console.error(err)});
 
 process.on("unhandledRejection", (reason, promise) => {
   console.log("Unhandled Rejection at:", promise, "reason:", reason);

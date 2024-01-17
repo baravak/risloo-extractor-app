9879f506c3dd9848791070f7c9f9da2b6625b8a6
abouhatef <itb.baravak@gmail.com>
2024-01-17 12:05:02 +0330

    JPFQ93 bugfix

--- src/samples/JPFQ93.js
+++ src/samples/JPFQ93.js
@@ -76,47 +76,46 @@ class JPFQ93 extends Profile {
   }
 }
 function clinicalScoring(items){
-    f1 = 'identity'
-    f2 = 'self_direction'
-    f3 = 'empathy'
-    f4 = 'intimacy'
-    f5 = 'psychoticism'
-    f6 = 'detachment'
-    f7 = 'disinhibition'
-    f8 = 'negative_affectivity'
-    f9 = 'antagonism'
-    factors = {
-        [`${f1}`] : [1,5,9,12,14,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,48,50,53],
-        [`${f2}`] : [2,6,10,15,20,26,32,38,44,49],
-        [`${f3}`] : [4,8,13,18,24,30,36,42,47,52,55],
-        [`${f4}`] : [3,7,11,16,22,28,34,40,46,51,54],
-        [`${f5}`] : [56,62,68,73,78,83,88,96,102,108,111],
-        [`${f6}`] : [60,67,72,77,82,87,94,100,106],
-        [`${f7}`] : [57,61,63,66,69,74,79,84,89,91,93,95,97,103,107,110,114,118,120,122,124,126],
-        [`${f8}`] : [59,65,71,76,81,86,92,99,105,112,116,119,121,123,125],
-        [`${f9}`] : [58,64,70,75,80,85,90,98,101,104,109,113,115,117]
-    }
-    reverse_scoring_numbers = [1,5,6,10,11,12,14,26,32,34,47,50,115]
-    result = {}
-    for(const f in factors){
-        const fItems = factors[f]
-        if(result[f] === undefined){
-            result[f] = {total: 0, percentage: 0, width: fItems.length * 2}
-        }
-        
-        fItems.forEach(index => {
-            answer = parseInt(items[index -1].user_answered)
+  f1 = 'identity'
+  f2 = 'self_direction'
+  f3 = 'empathy'
+  f4 = 'intimacy'
+  f5 = 'psychoticism'
+  f6 = 'detachment'
+  f7 = 'disinhibition'
+  f8 = 'negative_affectivity'
+  f9 = 'antagonism'
+  factors = {
+      [`${f1}`] : [1,5,9,12,14,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,48,50,53],
+      [`${f2}`] : [2,6,10,15,20,26,32,38,44,49],
+      [`${f3}`] : [4,8,13,18,24,30,36,42,47,52,55],
+      [`${f4}`] : [3,7,11,16,22,28,34,40,46,51,54],
+      [`${f5}`] : [56,62,68,73,78,83,88,96,102,108,111],
+      [`${f6}`] : [60,67,72,77,82,87,94,100,106],
+      [`${f7}`] : [57,61,63,66,69,74,79,84,89,91,93,95,97,103,107,110,114,118,120,122,124,126],
+      [`${f8}`] : [59,65,71,76,81,86,92,99,105,112,116,119,121,123,125],
+      [`${f9}`] : [58,64,70,75,80,85,90,98,101,104,109,113,115,117]
+  }
+  reverse_scoring_numbers = [1,5,6,10,11,12,14,26,32,34,47,50,115]
+  result = {}
+  for(const f in factors){
+      const fItems = factors[f]
+      if(result[f] === undefined){
+          result[f] = {total: 0, percentage: 0, width: fItems.length * 2}
+      }
+      fItems.forEach(index => {
+          answer = parseInt(items[index -1].user_answered)
           let incScore = 0
-            if(reverse_scoring_numbers.indexOf(index) === -1){
-                incScore = Math.floor((4 - answer)/1.5)
-            }else{
-                incScore = Math.floor((answer - 1)/1.5)
-            }
-            result[f].total = result[f].total + incScore
-        })
-        totalScore = fItems.length * 2
-        result[f].percentage = Math.round((result[f].total * 100) / totalScore)
-    }
-    return result
+          if(reverse_scoring_numbers.indexOf(index) === -1){
+              incScore = Math.floor((answer - 1)/1.5)
+          }else{
+              incScore = Math.floor((4 - answer)/1.5)
+          }
+          result[f].total = result[f].total + incScore
+      })
+      totalScore = fItems.length * 2
+      result[f].percentage = Math.round((result[f].total * 100) / totalScore)
+  }
+  return result
 }
 module.exports = JPFQ93;
